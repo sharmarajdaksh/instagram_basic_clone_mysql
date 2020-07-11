01fc11ccbc65ad9ff71ff5b2438f62d1e3ec4402
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2020-07-11 15:08:19 +0530

    Removed inserts from sql file.

--- instagram.sql
+++ instagram.sql
@@ -60,48 +60,48 @@ CREATE TABLE photo_tags (
     PRIMARY KEY(photo_id, tag_id)
 );
 
-INSERT INTO users (username) 
-VALUES 
-    ('sharmarajdaksh'),
-    ('dexs'),
-    ('dakshraj.sharma');
+-- INSERT INTO users (username) 
+-- VALUES 
+--     ('sharmarajdaksh'),
+--     ('dexs'),
+--     ('dakshraj.sharma');
 
-INSERT INTO photos (image_url, user_id)
-VALUES
-    ('/alskjd7', 1),
-    ('/lajsk98', 2),
-    ('/saud902', 2);
+-- INSERT INTO photos (image_url, user_id)
+-- VALUES
+--     ('/alskjd7', 1),
+--     ('/lajsk98', 2),
+--     ('/saud902', 2);
 
-INSERT INTO comments (comment_text, user_id, photo_id)
-VALUES
-    ('Whoa!', 1, 2),
-    ('Amazing Shot!', 3, 2),
-    ('I <3 This', 2, 1);
+-- INSERT INTO comments (comment_text, user_id, photo_id)
+-- VALUES
+--     ('Whoa!', 1, 2),
+--     ('Amazing Shot!', 3, 2),
+--     ('I <3 This', 2, 1);
 
-INSERT INTO likes(user_id, photo_id)
-VALUES
-    (1, 1),
-    (2, 1),
-    (1, 2),
-    (1, 3),
-    (3, 3);
+-- INSERT INTO likes(user_id, photo_id)
+-- VALUES
+--     (1, 1),
+--     (2, 1),
+--     (1, 2),
+--     (1, 3),
+--     (3, 3);
     
-INSERT INTO follows(follower_id, followee_id)
-VALUES
-    (1, 2),
-    (1, 3),
-    (3, 1),
-    (2, 3);
+-- INSERT INTO follows(follower_id, followee_id)
+-- VALUES
+--     (1, 2),
+--     (1, 3),
+--     (3, 1),
+--     (2, 3);
     
-INSERT INTO tags(tag_name)
-VALUES
-    ('adorable'),
-    ('cute'),
-    ('sunrise');
+-- INSERT INTO tags(tag_name)
+-- VALUES
+--     ('adorable'),
+--     ('cute'),
+--     ('sunrise');
     
-INSERT INTO photo_tags (photo_id, tag_id)
-VALUES
-    (1, 1),
-    (1, 2),
-    (2, 3),
-    (3, 2);
+-- INSERT INTO photo_tags (photo_id, tag_id)
+-- VALUES
+--     (1, 1),
+--     (1, 2),
+--     (2, 3),
+--     (3, 2);
